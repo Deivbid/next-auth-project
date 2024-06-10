2ab40bf6101caa1eb6c9b311ee862b78599e5cf5
David Aparicio <davidaapariciobak@gmail.com>
2024-06-10 17:21:53 -0300

    Feature: start implementing auth.js with credentials provider

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,35 @@
 "use server";
-import * as z from 'zod';
-import { RegisterSchema } from '@/schemas';
+import * as z from "zod";
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
+import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
+  const validatedFields = RegisterSchema.safeParse(values);
 
-    if(!validatedFields.success) {
-        return {error: "Invalid fields!"};
-    }
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
 
-    return { success: "Email Sent!" };
-}
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use " };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: SEND VERIFICATION TOKEN EMAIL
+
+  return { success: "Email Sent!" };
+};
