fa160994f0339d10334cc5a7ef9fc3ebf088a0d7
David Aparicio <davidaapariciobak@gmail.com>
2024-06-19 15:52:30 -0300

    Feature: Adding a server action vs api functions and show message for admins!

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { admin } from "@/actions/admin";
+import { RoleGate } from "@/components/Auth/RoleGate";
+import { FormSuccess } from "@/components/FormSuccess";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
+
+const AdminPage = () => {
+  const onServerActionClick = () => {
+    admin().then((response) => {
+      if (response.error) {
+        toast.error(response.error);
+      }
+
+      if (response.success) {
+        toast.success(response.success);
+      }
+    });
+  };
+
+  const onApiRouteClick = () => {
+    fetch("/api/admin").then((response) => {
+      if (response.ok) {
+        toast.success("Allowed Api Route");
+      } else {
+        toast.error("Forbbiden Api Route");
+      }
+    });
+  };
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">🔑 Admin</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="You are allowed to see this" />
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only api route</p>
+          <Button onClick={onApiRouteClick}>Click to test</Button>
+        </div>
+
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only server action</p>
+          <Button onClick={onServerActionClick}>Click to test</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AdminPage;
